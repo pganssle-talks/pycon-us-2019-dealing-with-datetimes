8d6ba4d62a9417c9acd5649d9f3928a61c92d6eb
Paul Ganssle <paul@ganssle.io>
2019-04-30 14:09:56 -0400

    Fix bus schedule tests

--- materials/03-recurring_events/rr_tests.py
+++ materials/03-recurring_events/rr_tests.py
@@ -34,7 +34,7 @@ def _test_bus_schedule(bus_schedule, exp_sched):
                  datetime(2021, 1, 31))
 
     act = bus_schedule.between(*dt_limits)
-    exp = bus_schedule.between(*dt_limits)
+    exp = exp_sched.between(*dt_limits)
 
     for dt_act, dt_exp in it.zip_longest(act, exp):
         assert dt_act == dt_exp
@@ -46,6 +46,7 @@ def test_basic_bus_schedule(bus_schedule):
 
     _test_bus_schedule(bus_schedule, exp_sched)
 
+
 def test_evening_bus_schedule(bus_schedule):
     exp_sched = rra.get_evening_schedule()
 
@@ -56,6 +57,7 @@ def test_no_election_day(bus_schedule):
     exp_sched = rra.get_no_election_schedule()
 
     _test_bus_schedule(bus_schedule, exp_sched)
+
 
 def test_final_schedule(bus_schedule):
     exp_sched = rra.get_final_schedule()
